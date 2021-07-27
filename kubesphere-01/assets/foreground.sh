c09910e4d8bf6d749cda9be951cb0500f50851b0
AbdelouahabMbarki <abdelouahab.mbarki1@gmail.com>
2021-07-27 12:29:41 +0000

    test execute

--- kubesphere-01/assets/foreground.sh
+++ kubesphere-01/assets/foreground.sh
@@ -1,3 +1,14 @@
+#!/bin/bash
+echo Waiting for Kubernetes to start...
+  while [ ! -f /root/.kube/config ]
+  do
+    sleep 1
+  done
+echo Kubernetes started
+if [ -f /root/.kube/start ]; then
+  /root/.kube/start
+fi
+
 helm repo add openebs https://openebs.github.io/charts
 
 helm repo update
